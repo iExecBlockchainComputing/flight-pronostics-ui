75f0b9d1fa521d87b0feeccff948b6635a18a3fe
SeddikBellamine <bellamine.seddik@gmail.com>
2021-10-08 01:49:09 +0100

    Bootstrap fix

---
+++ build/precache-manifest.0669baa5b3db5ddfb80fd39ae320509b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6360a1bbc343e687f4600c6634277b20",
+    "url": "/flight-pronostics-ui/index.html"
+  },
+  {
+    "revision": "e3ad4d4e4987fe99fb1b",
+    "url": "/flight-pronostics-ui/static/css/2.ecfb3f68.chunk.css"
+  },
+  {
+    "revision": "420312a62c86a45249c8",
+    "url": "/flight-pronostics-ui/static/css/main.c635cb7a.chunk.css"
+  },
+  {
+    "revision": "e3ad4d4e4987fe99fb1b",
+    "url": "/flight-pronostics-ui/static/js/2.f9d57a8f.chunk.js"
+  },
+  {
+    "revision": "f48cf4a1ba57a18ac5a4b5653897a604",
+    "url": "/flight-pronostics-ui/static/js/2.f9d57a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420312a62c86a45249c8",
+    "url": "/flight-pronostics-ui/static/js/main.fe7242a7.chunk.js"
+  },
+  {
+    "revision": "2990065f5d99f648e561",
+    "url": "/flight-pronostics-ui/static/js/runtime-main.4ea89584.js"
+  }
+]);
